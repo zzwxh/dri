bd99b630357c51f53c299ca003889ab2bac52503
zzwxh <zzwxh123@gmail.com>
2023-10-15 14:24:25 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,8 @@
 use std::process::Command;
 
-use anyhow::{ensure, Ok, Result};
+use anyhow::{ensure, Result};
 use clap::{Parser, Subcommand};
 use serde::Deserialize;
-
-fn encode(input: &str) -> Result<String> {
-    ensure!(input.len() <= 20);
-    let mut out = Vec::new();
-    for b in input.as_bytes() {
-        out.push((b & 0x0F) + 0x61);
-        out.push(((b & 0xF0) >> 4) + 0x61);
-    }
-    Ok(String::from_utf8(out).unwrap())
-}
-
-fn decode(input: &str) -> Result<String> {
-    ensure!(input.len() <= 40);
-    ensure!(input.len() % 2 == 0);
-    let mut out = Vec::new();
-    for b in input.as_bytes().chunks(2) {
-        out.push((b[0] - 0x61) | (b[1] - 0x61) << 4);
-    }
-    Ok(String::from_utf8(out)?)
-}
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -32,126 +12,94 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Cmd {
-    List,
-    Run {
-        #[arg(short, long)]
-        image: Option<String>,
-        #[arg(short, long)]
-        name: String,
-        #[arg(short, long)]
-        port: u16,
-    },
-    Stop {
-        #[arg(short, long)]
-        name: String,
-    },
-    Save {
-        #[arg(short, long)]
-        name: String,
-    },
-    Remove {
-        #[arg(short, long)]
-        name: String,
-    },
+    New { name: String, port: u16 },
+    Run { name: String, port: u16 },
+    Stop { name: String },
+    Rm { name: String },
+    Ls {},
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
     match cli.cmd {
-        Cmd::List => {
-            let images: Vec<Image> = image_list()?
-                .into_iter()
-                .filter(|i| i.name != "default")
-                .collect();
-            let containers = container_list()?;
-            println!("TYPE   NAME                 SIZE");
-            for i in images {
-                println!(
-                    "I      {:<20} {:<20}",
-                    decode(&i.name)?,
-                    i.size as f32 / 1048576f32
-                );
-            }
-            for c in containers {
-                println!(
-                    "C     {:<20} {:<20}",
-                    decode(&c.name)?,
-                    c.size as f32 / 1048576f32
-                );
-            }
+        Cmd::New { name, port } => {
+            run_podman(&[
+                "container",
+                "run",
+                "--rm",
+                "--detach",
+                "--publish",
+                &encode_port(port),
+                "--name",
+                &encode_container_name(&name),
+                "localhost/drix/default:latest",
+            ])?;
         }
-        Cmd::Run { image, name, port } => {
-            let image = match image {
-                Some(s) => encode(&s)?,
-                None => "default".to_string(),
-            };
-            let name = encode(&name)?;
-            ensure!(is_image(&image)?);
-            ensure!(!is_container(&name)?);
-            container_run(&image, &name, port)?;
+        Cmd::Run { name, port } => {
+            run_podman(&[
+                "container",
+                "run",
+                "--rm",
+                "--detach",
+                "--publish",
+                &encode_port(port),
+                "--name",
+                &encode_container_name(&name),
+                &encode_image_name(&name),
+            ])?;
         }
         Cmd::Stop { name } => {
-            let name = encode(&name)?;
-            ensure!(is_container(&name)?);
-            container_stop(&name)?;
+            run_podman(&[
+                "container",
+                "commit",
+                "--pause",
+                &encode_container_name(&name),
+                &encode_image_name(&name),
+            ])?;
+            run_podman(&["container", "stop", &encode_container_name(&name)])?;
         }
-        Cmd::Save { name } => {
-            let name = encode(&name)?;
-            ensure!(is_container(&name)?);
-            container_save(&name)?;
+        Cmd::Rm { name } => {
+            run_podman(&["image", "rm", &encode_image_name(&name)])?;
         }
-        Cmd::Remove { name } => {
-            let name = encode(&name)?;
-            ensure!(is_image(&name)?);
-            ensure!(!is_container(&name)?);
-            image_remove(&name)?;
+        Cmd::Ls {} => {
+            container_list()?.iter().for_each(|c| {
+                println!("{:?}", c);
+            });
+            image_list()?.iter().for_each(|i| {
+                println!("{:?}", i);
+            });
         }
     }
     Ok(())
 }
 
-fn image_remove(name: &str) -> Result<()> {
-    run_podman(&["image", "rm", &format!("localhost/dri/{}", name)])?;
-    Ok(())
+fn encode_port(port: u16) -> String {
+    format!("127.0.0.1:{}:22", port)
 }
 
-fn container_save(name: &str) -> Result<()> {
-    run_podman(&[
-        "container",
-        "commit",
-        "--pause",
-        &format!("dri-{}", name),
-        &format!("localhost/dri/{}", name),
-    ])?;
-    Ok(())
+fn encode_container_name(name: &str) -> String {
+    format!("dri-{}", encode(name).unwrap())
 }
 
-fn container_stop(name: &str) -> Result<()> {
-    run_podman(&["container", "stop", "--time", "2", &format!("dri-{}", name)])?;
-    Ok(())
+fn parse_container_name(name: &str) -> Option<String> {
+    match name.strip_prefix("dri-") {
+        Some(name) => decode(name).ok(),
+        None => None,
+    }
 }
 
-fn is_image(name: &str) -> Result<bool> {
-    Ok(image_list()?.iter().any(|i| i.name == name))
+fn encode_image_name(name: &str) -> String {
+    format!("localhost/dri/{}:latest", encode(name).unwrap())
 }
 
-fn is_container(name: &str) -> Result<bool> {
-    Ok(container_list()?.iter().any(|c| c.name == name))
-}
-
-fn container_run(image: &str, name: &str, port: u16) -> Result<()> {
-    run_podman(&[
-        "container",
-        "run",
-        "--rm",
-        "--detach",
-        "--publish",
-        &format!("127.0.0.1:{}:22", port),
-        "--name",
-        &format!("dri-{}", name),
-        &format!("localhost/dri/{}", image),
-    ])?;
-    Ok(())
+fn parse_image_name(name: &str) -> Option<String> {
+    match name.strip_prefix("localhost/dri/") {
+        Some(name) => match name.strip_suffix(":latest") {
+            Some(name) => decode(name).ok(),
+            None => None,
+        },
+        None => None,
+    }
 }
 
 fn container_list() -> Result<Vec<Container>> {
@@ -160,8 +108,8 @@ fn container_list() -> Result<Vec<Container>> {
     let vec = raw_vec
         .into_iter()
         .filter_map(|raw| {
-            raw.names[0].strip_prefix("dri-").map(|s| Container {
-                name: s.to_string(),
+            parse_container_name(&raw.names[0]).map(|name| Container {
+                name,
                 size: raw.size.root_fs_size,
             })
         })
@@ -195,13 +143,10 @@ fn image_list() -> Result<Vec<Image>> {
     let vec = raw_vec
         .into_iter()
         .filter_map(|raw| {
-            raw.names[0]
-                .strip_prefix("localhost/dri/")
-                .map(|s| s.strip_suffix(":latest").unwrap())
-                .map(|s| Image {
-                    name: s.to_string(),
-                    size: raw.size,
-                })
+            parse_image_name(&raw.names[0]).map(|name| Image {
+                name,
+                size: raw.size,
+            })
         })
         .collect();
     Ok(vec)
@@ -232,4 +177,24 @@ fn run_podman(args: &[&str]) -> Result<String> {
         stderr,
     );
     Ok(stdout)
+}
+
+fn encode(input: &str) -> Result<String> {
+    ensure!(input.len() <= 20);
+    let mut out = Vec::new();
+    for b in input.as_bytes() {
+        out.push((b & 0x0F) + 0x61);
+        out.push(((b & 0xF0) >> 4) + 0x61);
+    }
+    Ok(String::from_utf8(out).unwrap())
+}
+
+fn decode(input: &str) -> Result<String> {
+    ensure!(input.len() <= 40);
+    ensure!(input.len() % 2 == 0);
+    let mut out = Vec::new();
+    for b in input.as_bytes().chunks(2) {
+        out.push((b[0] - 0x61) | (b[1] - 0x61) << 4);
+    }
+    Ok(String::from_utf8(out)?)
 }
